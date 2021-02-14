2fd300672042ebec91dee745cdc8e7f2e9e4dcf0
Marcelo Tonhetti Junior <50048511+MarceloTonhetti@users.noreply.github.com>
2021-02-14 13:31:25 -0300

    Implementando a leitura e impressão de dados e a função para verificar se a posição está livre.

--- tic-tac-toe/tic-tac-toe/Program.cs
+++ tic-tac-toe/tic-tac-toe/Program.cs
@@ -10,9 +10,81 @@ namespace tic_tac_toe
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[,] board = new int[3,3];
+			int player = 1;
+			int line=0, column=0;
 
+            
+
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Jogo da velha!!!\n\n");
+                
+                Console.WriteLine("Partida em andamento...");
+				imprimir_jogo(board);
+
+				if(player % 2 != 0)
+					readChoice(board, 1, ref line, ref column);
+				else
+					readChoice(board, 4, ref line, ref column);
+
+                
+
+            
+
+				player++;
+			}while(board[2,2] == 0);
+        
+			Console.WriteLine("Pressione qualquer tecla para sair...");
 			Console.ReadKey();
 		}
+
+		static void readChoice(int[,] board, int player, ref int line, ref int column)
+		{
+			bool flag = false;
+
+			do
+			{
+				Console.WriteLine("\nJogador " + player);
+				Console.Write("Escolha a posição da linha: ");
+				line = int.Parse(Console.ReadLine());
+				Console.Write("Escolha a posição da coluna: ");
+				column = int.Parse(Console.ReadLine());
+
+				if(verificaPosicao(board, line, column))
+				{
+					board[line, column] = player;
+					flag = false;
+				}
+				else
+					flag = true;
+					
+			}while(flag);
+		}
+
+		static void imprimir_jogo(int[,] board)
+		{
+			Console.WriteLine("\n");
+			Console.WriteLine("    0   1   2 \n");
+			Console.WriteLine("0   "+board[0,0]+" | "+board[0,1]+" | "+board[0,2]);
+			Console.WriteLine("   -----------");
+			Console.WriteLine("1   "+board[1,0]+" | "+board[1,1]+" | "+board[1,2]);
+			Console.WriteLine("   -----------");
+			Console.WriteLine("2   "+board[2,0]+" | "+board[2,1]+" | "+board[2,2]);
+		}
+
+
+
+
+
+		static bool verificaPosicao(int[,] board, int line, int column )
+		{
+			if(board[line, column] == 0 )
+				return true;
+			else
+				return false;
+		}
+
 	}
 }
